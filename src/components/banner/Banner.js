c8f0f64640a532f60bb5e7c58d58a0d5ed4c0747
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-12-14 15:54:26 +0000

    Make navbar responsive and add content to homepage

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,7 +1,33 @@
+import "./banner.css";
+
 const Banner = () => {
   return (
     <div className="container">
-      <h1>Banner</h1>
+      <div className="section">
+        <div className="description">
+          <p>Hello!</p>
+          <h1>I am Kaisa</h1>
+          <p>a web developer</p>
+        </div>
+      </div>
+      <div className="section">
+        <div className="triangles">
+          <div className="triangle triangle-up">
+            <div className="triangle-up-front"></div>
+            <div className="triangle-up-back"></div>
+          </div>
+          <div className="triangle triangle-down">
+            <div className="triangle-down-front"></div>
+            <div className="triangle-down-back"></div>
+          </div>
+        </div>
+        <div className="quote-box">
+          <p>
+            <i class="fas fa-quote-left fa-2x" /> Love what you do,
+          </p>
+          <p>Do what you love</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,26 +1,58 @@
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../images/pinkK.png";
 import "./navbar.css";
 
 const Navbar = () => {
+  const [screenSize, setScreenSize] = useState(null);
+  const [activeMenu, setActiveMenu] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize < 700) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
+
+  const handleHamburgerMenuClick = () => {
+    setActiveMenu(!activeMenu);
+  };
+
   return (
-    <div className="navbar">
-      <Link to="/">
-        <img src={logo} className="logo" alt="logo" />
-      </Link>
-      {/* <div className="new-logo">K</div> */}
-      <div className="nav-options">
-        <Link to="/about" className="nav-option">
-          About
+    <nav className="navbar">
+      <div className="logo-container">
+        <Link to="/">
+          <img src={logo} className="logo" alt="logo" />
         </Link>
-        <Link to="/projects" className="nav-option">
-          Projects
-        </Link>
-        <Link to="/contact" className="nav-option">
-          Contact
-        </Link>
+        <div className="menu-control" onClick={handleHamburgerMenuClick}>
+          <i className="fas fa-bars fa-lg" />
+        </div>
       </div>
-    </div>
+      {activeMenu && (
+        <div className="nav-options">
+          <Link to="/" className="nav-option">
+            Home
+          </Link>
+          <Link to="/about" className="nav-option">
+            About
+          </Link>
+          <Link to="/projects" className="nav-option">
+            Projects
+          </Link>
+          <Link to="/contact" className="nav-option">
+            Contact
+          </Link>
+        </div>
+      )}
+    </nav>
   );
 };
 
