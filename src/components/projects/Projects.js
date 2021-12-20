f4cdd706cbd0014d5c996a880829ea8561e3555b
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-12-20 16:21:31 +0000

    Create initial dataset for projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,28 +1,18 @@
 import "./projects.css";
+import projectArr from "../../data/projectArr.json";
 
 const Projects = () => {
+  console.log(projectArr);
   return (
     <div className="container">
       <h1 className="title">Projects</h1>
       <div className="project-grid">
-        <div className="project">
-          <h3>DoggyCare</h3>
-        </div>
-        <div className="project">
-          <h3>Cryptocurrency Web</h3>
-        </div>
-        <div className="project">
-          <h3>Memory Game</h3>
-        </div>
-        <div className="project">
-          <h3>Movie Search</h3>
-        </div>
-        <div className="project">
-          <h3>Face Recognition App</h3>
-        </div>
-        <div className="project">
-          <h3>Weather App</h3>
-        </div>
+        {projectArr.map((project) => (
+          <div className="project" key={project.name}>
+            <h3>{project.name}</h3>
+            <p>{project.description}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
