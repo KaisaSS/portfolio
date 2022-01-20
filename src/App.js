a317bb78fa27671bd1b4acd75f6cc92724d3d347
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2022-01-20 11:22:42 +0000

    Page auto scroll to top on page refresh

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from "./components/footer/Footer";
 import Home from "./components/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Projects from "./components/projects/Projects";
+import ScrollToTop from "./components/navbar/ScrollToTop";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Navbar />
         </div>
         <div>
+          <ScrollToTop />
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
