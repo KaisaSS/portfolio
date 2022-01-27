3d581cdd8398484e0cd4b09d1c5b075893477e5b
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2022-01-27 15:25:42 +0000

    Modify project selection for mobile devices

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -19,23 +19,37 @@ const Projects = () => {
         <h1 className="title">Projects</h1>
         <div className="project-grid">
           {projectArr.map((project) => (
-            <div className="project" key={project.name}>
+            <div
+              className="project"
+              key={project.name}
+              onClick={
+                window.innerWidth < 800
+                  ? () => {
+                      showSingleProject(project);
+                    }
+                  : null
+              }
+            >
               <h3>{project.name}</h3>
               <div className="project-cover">
                 <img src={project.coverPhoto} alt="cover" />
               </div>
               <p className="project-description">{project.shortDescription}</p>
-              <div
-                className="tooltip"
-                onClick={() => {
-                  showSingleProject(project);
-                }}
-              >
-                <button>
-                  <i className="fas fa-angle-double-right fa-2x" />
-                </button>
-                <span className="tooltiptext">See more</span>
-              </div>
+              {window.innerWidth > 800 ? (
+                <div
+                  className="tooltip"
+                  onClick={() => {
+                    showSingleProject(project);
+                  }}
+                >
+                  <button>
+                    <i className="fas fa-angle-double-right fa-2x" />
+                  </button>
+                  <span className="tooltiptext">See more</span>
+                </div>
+              ) : (
+                <div></div>
+              )}
             </div>
           ))}
         </div>
